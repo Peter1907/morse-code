6e12b76267057044e43dd52245d6351f09db2997
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-11-09 19:36:16 -0300

    Fix linter error and make uper cases to the method

--- decoder.rb
+++ decoder.rb
@@ -1,51 +1,51 @@
+@code = {
+  '.-' =>    'a',
+  '-...' =>  'b',
+  '-.-.' =>  'c',
+  '-..' =>   'd',
+  '.' =>     'e',
+  '..-.' =>  'f',
+  '--.' =>   'g',
+  '....' =>  'h',
+  '..' =>    'i',
+  '.---' =>  'j',
+  '-.-' =>   'k',
+  '.-..' =>  'l',
+  '--' =>    'm',
+  '-.' =>    'n',
+  '---' =>   'o',
+  '.--.' =>  'p',
+  '--.-' =>  'q',
+  '.-.' =>   'r',
+  '...' =>   's',
+  '-' =>     't',
+  '..-' =>   'u',
+  '...-' =>  'v',
+  '.--' =>   'w',
+  '-..-' =>  'x',
+  '-.--' =>  'y',
+  '--..' =>  'z'
+}
 
 def decode(string)
-  code = { 
-    '.-' =>    'a',
-    '-...' =>  'b',
-    '-.-.' =>  'c',
-    '-..' =>   'd',
-    '.' =>     'e',
-    '..-.' =>  'f',
-    '--.' =>   'g',
-    '....' =>  'h',
-    '..' =>    'i',
-    '.---' =>  'j',
-    '-.-' =>   'k',
-    '.-..' =>  'l',
-    '--' =>    'm',
-    '-.' =>    'n',
-    '---' =>   'o',
-    '.--.' =>  'p',
-    '--.-' =>  'q',
-    '.-.' =>   'r',
-    '...' =>   's',
-    '-' =>     't',
-    '..-' =>   'u',
-    '...-' =>  'v',
-    '.--' =>   'w',
-    '-..-' =>   'x',
-    '-.--' =>  'y',
-    '--..' =>  'z',
-  }
   coded_words = string.split('   ')
   coded_letters = []
   letters = []
   words = []
-  result = ""
+  result = ''
   coded_words.each do |word|
-    coded_letters = word.split(' ')
+    coded_letters = word.split
     coded_letters.each do |letter|
-      letters << code[letter]
+      letters << @code[letter]
     end
     words << letters
     letters = []
     coded_letters = []
   end
   words.each do |word|
-    result += "#{word.join('')}\s"
+    result += '#{word.join}\s'
   end
-  print result.strip
+  print result.strip.upcase
 end
 
-print decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
